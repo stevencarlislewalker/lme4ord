d528b04ebbc92f4d89255c22f9540d4d44699879
stevencarlislewalker <scwalker@math.mcmaster.ca>
2015-02-11 16:36:43 -0500

    working on some things

--- inst/misc/gblmer.R
+++ inst/misc/gblmer.R
@@ -6,60 +6,62 @@ library(multitable)
 library(pryr)
 library(reo)
 
-## n <- 50
-## m <- 30
-## x <- rnorm(n)
-## z <- rnorm(m)
-## B <- rmat(2, 2)
-## u <- rnorm(n)
-## v <- rnorm(m)
+n <- 50
+m <- 30
+x <- rnorm(n)
+z <- rnorm(m)
+B <- rmat(2, 2)
+u <- rnorm(n)
+v <- rnorm(m)
 
-## eta <-
-##     (cbind(1, x) %*% B %*% rbind(1, z)) +
-##     (cbind(1, x) %*%        rmat(2, m)) +
-##     ( rmat(n, 2) %*%       rbind(1, z)) +
-##     (   cbind(u) %*%          rbind(v))
+eta <-
+    (cbind(1, x) %*% B %*% rbind(1, z)) +
+    (cbind(1, x) %*%        rmat(2, m)) +
+    ( rmat(n, 2) %*%       rbind(1, z)) +
+    (   cbind(u) %*%          rbind(v))
 
-## Y <- rmat(n, m,
-##           f(rbinom(n, size = 1, prob = plogis(eta))),
-##           eta = eta)
+Y <- rmat(n, m,
+          f(rbinom(n, size = 1, prob = plogis(eta))),
+          eta = eta)
+
 
 data(fish)
 data(limn)
 Y <- as.matrix(fish)
-## Y <- Y[, colSums(Y) > 1]
 n <- nrow(Y)
 m <- ncol(Y)
 x <- as.vector(scale(limn$pH))
+
 dl <- data.list(Y = Y, x = x,
                 dimids = c("sites", "species"))
 summary(dl)
 
-mod <- gblmer(Y ~ 1 + (1 | species), dl, binomial,
-              loadingsDim = 2,
-              latentDim = 2,
-              loadingPen = 1L,
-              verbose = 2L)
+mod00 <- gblmer(Y ~ 1 + (1 | species), dl, binomial,
+                loadingsDim = 0)
 
-## dl <- dl + variable(1:dim(dl)[1], "species", "latent")
-## df <- as.data.frame(dims_to_vars(dl))
-##   linFormula <- Y ~ 1 + (1          | species)
-## bilinFormula <- Y ~ 0 + (0 + latent | sites  )
-##   parsedLinFormula <- glFormula(  linFormula, df, binomial)
-## parsedBilinFormula <- glFormula(bilinFormula, df, binomial)
-## reTrms <- joinReTrms(parsedBilinFormula$reTrms, parsedLinFormula$reTrms)
-## dfun <- mkGblmerDevfun(parsedLinFormula$fr, parsedLinFormula$X,
-##                          parsedLinFormula$reTrms,
-##                        parsedBilinFormula$reTrms, binomial)
-## rho <- environment(dfun)
-## rho$pp$Zt@i %in% (seq_len(rho$q[1]) - 1)
+mod01 <- gblmer(Y ~ x + (x | species), dl, binomial,
+                loadingsDim = 0)
 
-## mod <- gblmer(Y ~ 1 + (1 | species), . ~ 0 + (0 + latent | sites),
-##               dl, binomial, 1, 1, 2)
+mod0 <- gblmer(Y ~ 1 + (1 | species), dl, binomial,
+               loadingsDim = 2,
+               latentDim = 2,
+               loadingPen = 1L,
+               verbose = 2L)
 
-mod
-summary(mod)
+mod1 <- gblmer(Y ~ x + (x | species), dl, binomial,
+               loadingsDim = 2,
+               latentDim = 2,
+               loadingPen = 1L,
+               verbose = 2L)
 
+mod00
+mod01
+mod0
+mod1
+summary(mod0)
+summary(mod1)
+
+mod <- mod0
 ranef(mod)$species
 ranef(mod)$sites
 (loadMod <- loadings(mod))
@@ -67,20 +69,36 @@ latentCov <- Matrix(loadMod %*% t(loadMod)) + diag(VarCorr(mod)$species[1], m, m
 image(cov2cor(latentCov))
 image(vcov(mod)@factors$correlation)
 
+mods <- list(mod00, mod01, mod0, mod1)
+fits <- lapply(mods, function(mm) matrix(getME(mm, "mu"), n, m))
+par(mfrow = c(2, 2))
+trash <- lapply(fits, function(ff) boxplot(ff ~ Y, las = 1, horizontal = TRUE))
 
-fitY <- matrix(getME(mod, "mu"), n, m, byrow = TRUE)
-boxplot(fitY ~ Y, las = 1, horizontal = TRUE)
+k <- "latent1"
+mod <- mod0
+b <- ranef(mod)$sites[, k]
+bb <- seq(min(b), max(b), length = 100)
+for(j in 1:m) {
+    plot(ranef(mod)$sites[ , k], Y[, j],
+         main = colnames(Y)[j])
+    lines(bb, plogis(loadings(mod)[j, k]*bb))
+    readline("press any key")
+}
 
-## j <- 3
-## plot(fitY[,j], Y[,j])
-
-# plot(u, getME(mod, "b")[1:n])
-# plot(v, mod@loadings)
-
-j <- 14
-b <- getME(mod, "b")[1:n]
+j <- 3
+k <- "latent2"
+mod <- mod1
+b <- ranef(mod)$sites[, k]
 bb <- seq(min(b), max(b), length = 100)
 plot(b, Y[,j])
-lines(bb, plogis(loadings(mod)[j]*bb))
+lines(bb, plogis(loadings(mod)[j, k]*bb))
+
+par(mfrow = c(1, 2))
+for(mod in c(mod0, mod1)) {
+    vm <- varimax(loadings(mod))
+    biplot(as.matrix(ranef(mod)$sites) %*% vm$rotmat,
+           vm$loadings, cex = 0.5)
+}
 
 
+plot(loadings(mod0), loadings(mod1))
