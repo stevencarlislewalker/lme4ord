ce1a4b916e811d185c7b2d4372908f903991e133
stevencarlislewalker <scwalker@math.mcmaster.ca>
2015-02-11 16:40:09 -0500

    R code for general devfuns

---
+++ inst/misc/generalDevfuns/generalDevfuns.R
@@ -0,0 +1,191 @@
+
+## ----prelim, message = FALSE, results = 'hide', echo = FALSE-------------
+library("lme4ord")
+
+
+## ----simulate params-----------------------------------------------------
+n <- 60 # samples 
+p <- 2  # fixed effects
+q <- 30 # random effects
+
+
+## ----cov fact sims-------------------------------------------------------
+covTemplate <- as(chol(cov(matrix(rnorm((q+1)*q), q + 1, q))),
+                  "sparseMatrix")
+Lambdat <- covTemplate
+
+
+## ----viewLambdat, fig.width = 4, fig.height = 4--------------------------
+image(Lambdat)
+
+
+## ----mod mat sims--------------------------------------------------------
+Zt <- sparseMatrix(i = rep(1:q, 6),
+                   j = as.vector(outer(rep(1:10, 3), seq(0, 50, 10), "+")),
+                   x = rep(rnorm(10), 18))
+X <- matrix(rnorm(n * p), n, p)
+
+
+## ----viewZt, fig.width = 5, fig.height = 3-------------------------------
+image(Zt)
+
+
+## ----resp vect sims------------------------------------------------------
+eta <- as.numeric(X %*% c(1, 1) + t(Zt) %*% t(Lambdat) %*% rnorm(q))
+y <- rbinom(n, 1, plogis(eta))
+weights <- rep(1, n); offset <- rep(0, n)
+
+
+## ----initial-------------------------------------------------------------
+initPars <- c(covar = 1,
+              fixef = c(0, 0),
+              loads = rnorm(10))
+
+
+## ----par indices---------------------------------------------------------
+parInds <- list(covar = 1,
+                fixef = 2:3,
+                loads = 4:13)
+
+
+## ----mappings------------------------------------------------------------
+mapToCovFact <- function(covar) covar * covTemplate@x
+mapToModMat <- function(loads) rep(loads, 18)
+
+
+## ----illustrate mappings-------------------------------------------------
+Lambdat@x <- mapToCovFact(initPars[parInds$covar])
+Zt@x <- mapToModMat(initPars[parInds$loads])
+
+
+## ----make the deviance function------------------------------------------
+devfun <- mkGeneralGlmerDevfun(y, X, Zt, Lambdat,
+                               weights, offset,
+                               initPars, parInds,
+                               mapToCovFact, mapToModMat)                               
+
+
+## ----use the dev funct---------------------------------------------------
+devfun(initPars)
+
+
+## ----optimize the deviance function--------------------------------------
+opt <- minqa:::bobyqa(initPars, devfun)
+
+
+## ----look at the optimal solution----------------------------------------
+setNames(opt$par, names(initPars))
+
+
+## ----more packages, message = FALSE--------------------------------------
+library(reo)
+library(multitable)
+
+
+## ----fish data-----------------------------------------------------------
+data(fish)
+data(limn)
+Y <- as.matrix(fish)
+n <- nrow(Y)
+m <- ncol(Y)
+x <- as.vector(scale(limn$pH))
+dl <- data.list(Y = Y, x = x,
+                dimids = c("sites", "species"))
+dl <- dims_to_vars(dl)
+summary(dl)
+
+
+## ----long----------------------------------------------------------------
+head(df <- as.data.frame(dl))
+
+
+## ----fish resp-----------------------------------------------------------
+y <- df$Y
+weights <- rep(1, length(y)); offset <- rep(0, length(y))
+
+
+## ----fish X--------------------------------------------------------------
+X <- model.matrix(Y ~ x, df)[,]
+
+
+## ----fish Zt-------------------------------------------------------------
+Jspecies <- t(as(as.factor(df$species), Class = "sparseMatrix"))
+Zt <- KhatriRao(t(Jspecies), t(X))
+
+
+## ----loadings matrix-----------------------------------------------------
+nFreeLoadings <- m
+U <- matrix(1:nFreeLoadings, nrow = m, ncol = 1)
+latentVarNames <- "latent"
+U <- setNames(as.data.frame(U), latentVarNames)
+latentData <- data.list(U, drop = FALSE, dimids = "species")
+df <- as.data.frame(dl + latentData)
+Jsites <- with(df, t(as(as.factor(sites), Class = "sparseMatrix")))
+Zt <- rBind(KhatriRao(t(Jsites), t(model.matrix(Y ~ 0 + latent, df))), Zt)
+
+
+## ----cov fac fish--------------------------------------------------------
+templateFact <- sparseMatrix(i = 1:n, j = 1:n, x = rep(1, n))
+ii <- rep(1:2, 1:2); jj <- sequence(1:2)
+templateRanef <- sparseMatrix(i = ii, j = jj, x = 1 * (ii == jj))
+Lambdat <- .bdiag(c(list(templateFact), 
+                    rep(list(templateRanef), m)))
+
+
+## ----loadings mapping fish-----------------------------------------------
+mapToModMat <- local({
+    Ztx <- Zt@x
+    Zwhich <- Zt@i %in% (seq_len(n) - 1)
+    Zind <- Zt@x[Zwhich]
+    loadInd <- 1:nFreeLoadings
+    function(loads) {
+        Ztx[Zwhich] <- loads[Zind]
+        return(Ztx)
+    }
+})
+
+
+## ----lambda mapping fish-------------------------------------------------
+mapToCovFact <- local({
+    Lambdatx <- Lambdat@x
+    LambdaWhich <- (n+1):length(Lambdatx)
+    LindTemplate <- ii + 2 * (jj - 1) - choose(jj, 2)
+    Lind <- rep(LindTemplate, m)
+    function(covar) {
+        Lambdatx[LambdaWhich] <- covar[Lind]
+        return(Lambdatx)
+    }
+})
+
+
+## ----pars fish-----------------------------------------------------------
+initPars <- c(covar = c(1, 0, 1),
+              fixef = c(0, 0),
+              loads = rep(0, m))
+parInds <- list(covar = 1:3,
+                fixef = 4:5,
+                loads = (1:m)+5)
+
+
+## ----make fish devfun, warning = FALSE-----------------------------------
+devfun <- mkGeneralGlmerDevfun(y, X, Zt, Lambdat,
+                               weights, offset, 
+                               initPars, parInds,
+                               mapToCovFact, mapToModMat)
+
+
+## ----opt fish, eval = TRUE-----------------------------------------------
+opt <- minqa:::bobyqa(initPars, devfun)
+setNames(opt$par, names(initPars))
+
+
+## ----ordination diagram, fig.width = 7, fig.height = 7-------------------
+siteScores <- cbind(pH = x,
+                    latent = environment(devfun)$pp$b(1)[1:n])
+speciesScores <- cbind(pH = environment(devfun)$pp$b(1)[-(1:(n+m))],  
+                       latent = opt$par[parInds$loads])
+rownames(siteScores) <- rownames(Y)
+rownames(speciesScores) <- colnames(Y)
+biplot(siteScores, speciesScores, cex = 0.5)
+
+
---
+++ inst/misc/generalDevfuns/playingAround.R
@@ -0,0 +1,153 @@
+library(lme4ord)
+library(multitable)
+library(ape)
+
+# Generate simulated data for nspp species and nsite sites
+nspp <- 15
+nsite <- 10
+
+# residual variance (set to zero for binary data)
+sd.resid <- 0
+
+# fixed effects
+beta0 <- 0
+beta1 <- 0
+
+# magnitude of random effects
+sd.B0 <- 1
+sd.B1 <- 1
+
+# whether or not to include phylogenetic signal in B0 and B1
+signal.B0 <- TRUE
+signal.B1 <- TRUE
+
+# simulate a phylogenetic tree
+phy <- rtree(n = nspp)
+phy <- compute.brlen(phy, method = "Grafen", power = 0.5)
+
+# standardize the phylogenetic covariance matrix to have determinant 1
+Vphy <- vcv(phy)
+Vphy <- Vphy/(det(Vphy)^(1/nspp))
+
+# Generate environmental site variable
+X <- matrix(1:nsite, nrow = 1, ncol = nsite)
+X <- (X - mean(X))/sd(X)
+
+# Perform a Cholesky decomposition of Vphy. This is used to
+# generate phylogenetic signal: a vector of independent normal random
+# variables, when multiplied by the transpose of the Cholesky
+# deposition of Vphy will have covariance matrix equal to Vphy.
+
+iD <- t(chol(Vphy))
+
+# Set up species-specific regression coefficients as random effects
+if (signal.B0 == TRUE) {
+		b0 <- beta0 + iD %*% rnorm(nspp, sd = sd.B0)
+} else {
+		b0 <- beta0 + rnorm(nspp, sd = sd.B0)
+}
+if (signal.B1 == TRUE) {
+		b1 <- beta1 + iD %*% rnorm(nspp, sd = sd.B1)
+} else {
+		b1 <- beta1 + rnorm(nspp, sd = sd.B1)
+}
+
+# Simulate species abundances among sites to give matrix Y that
+# contains species in rows and sites in columns
+y <- matrix(outer(b0, array(1, dim = c(1, nsite))), nrow = nspp,
+ncol = nsite) + matrix(outer(b1, X), nrow = nspp, ncol = nsite)
+e <- rnorm(nspp * nsite, sd = sd.resid)
+y <- y + matrix(e, nrow = nspp, ncol = nsite)
+y <- matrix(y, nrow = nspp * nsite, ncol = 1)
+
+Y <- rbinom(n = length(y), size = 1, prob = exp(y)/(1 + exp(y)))
+Y <- matrix(Y, nrow = nspp, ncol = nsite)
+
+# name the simulated species 1:nspp and sites 1:nsites
+rownames(Y) <- 1:nspp
+colnames(Y) <- 1:nsite
+
+par(mfrow = c(3, 1), las = 1, mar = c(2, 4, 2, 2) - 0.1)
+matplot(t(X), type = "l", ylab = "X", main = "X among sites")
+hist(b0, xlab = "b0", main = "b0 among species")
+hist(b1, xlab = "b1", main = "b1 among species")
+
+par(mfrow = c(1, 1), las = 1, mar = c(4, 4, 2, 2) - 0.1)
+if(require(plotrix))
+    color2D.matplot(Y, ylab = "species", xlab = "sites", main = "abundance")
+
+# Transform data matrices into "long" form, and generate a data frame
+YY <- matrix(Y, nrow = nspp * nsite, ncol = 1)
+
+XX <- matrix(kronecker(X, matrix(1, nrow = nspp, ncol = 1)), nrow =
+nspp * nsite, ncol = 1)
+
+site <- matrix(kronecker(1:nsite, matrix(1, nrow = nspp, ncol =
+1)), nrow = nspp * nsite, ncol = 1)
+sp <- matrix(kronecker(matrix(1, nrow = nsite, ncol = 1), 1:nspp),
+nrow = nspp * nsite, ncol = 1)
+
+dat <- data.frame(Y = YY, X = XX, site = as.factor(site), sp = as.factor(sp))
+
+# Format input and perform communityPGLMM()
+# set up random effects
+
+# random intercept with species independent
+re.1 <- list(1, sp = dat$sp, covar = diag(nspp))
+
+# random intercept with species showing phylogenetic covariances
+re.2 <- list(1, sp = dat$sp, covar = Vphy)
+
+# random slope with species independent
+re.3 <- list(dat$X, sp = dat$sp, covar = diag(nspp))
+
+# random slope with species showing phylogenetic covariances
+re.4 <- list(dat$X, sp = dat$sp, covar = Vphy)
+
+# random effect for site
+re.site <- list(1, site = dat$site, covar = diag(nsite))
+
+dl <- dims_to_vars(data.list(Y = t(Y), X = as.numeric(X),
+                             dimids = c("sites", "species")))
+df <- as.data.frame(dl)
+df$sites <- factor(df$sites, levels = unique(as.character(df$sites)))
+df$species <- factor(df$species, levels = unique(as.character(df$species)))
+
+y <- df$Y
+weights <- rep(1, length(y)); offset <- rep(0, length(y))
+
+M <- model.matrix(Y ~ X, df)
+
+Jspecies <- as(df$species, "sparseMatrix")
+
+                            
+Ztlist <- list(KhatriRao(Jspecies, t(M[,1])),
+               KhatriRao(Jspecies, t(M[,1])),
+               KhatriRao(Jspecies, t(M[,2])),
+               KhatriRao(Jspecies, t(M[,2])))
+Zt <- do.call(rBind, Ztlist)
+
+Lambdatlist <- list(as(t(chol(re.1$covar)), Class="sparseMatrix"),
+                    as(t(chol(re.2$covar)), Class="sparseMatrix"),
+                    as(t(chol(re.3$covar)), Class="sparseMatrix"),
+                    as(t(chol(re.4$covar)), Class="sparseMatrix"))
+Lambdat <- .bdiag(Lambdatlist)
+
+Zt <- Zt[46:60, ]
+Lambdat <- Lambdat[46:60, 46:60]
+
+image(as(kronecker(matrix(1, 10, 15), t(chol(re.2$covar))), Class = "sparseMatrix"))
+
+image(Lambdat)
+image(Zt)
+image(Lambdat %*% Zt %*% t(Zt) %*% t(Lambdat))
+image(t(Zt) %*% t(Lambdat) %*% Lambdat %*% Zt)
+
+mapToModMat <- local({
+    Ztx <- Zt@x
+    function(loads) Ztx
+})
+
+mapToCovFact <- local({
+    
+})
