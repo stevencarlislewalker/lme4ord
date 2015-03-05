242ff6e4159ff4f076eb98ddae4c0952569e9dd4
stevencarlislewalker <scwalker@math.mcmaster.ca>
2015-03-05 08:45:13 -0500

    old experimentation with readme

--- inst/README/README.R
+++ inst/README/README.R
@@ -25,8 +25,8 @@ library(reo)
 
 ## ------------------------------------------------------------------------
 set.seed(10)
-n <- 100
-m <- 10000
+n <- 95 # 100
+m <- 475 # 500
 dl <- dims_to_vars(data.list(y = 1 * (matrix(rnorm(n * m), n, m) > 0),
                              x = rnorm(n), z = rnorm(m),
                              dimids = c("sites", "species")))
@@ -91,10 +91,77 @@ color2D.matplot(dl$y, xlab = "species", ylab = "sites", main = "abundance")
 
 ## ------------------------------------------------------------------------
 system.time({
-    (mod <- glmerc(form, df, covMat = covMat))
+    (mod <- glmerc(form, df, covMat = covMat,
+                   optControl = list(iprint = 4L, maxfun = 500)))
 })
 
+centDfun <- function(par) mod$dfun(par) - mod$opt$fval
+hh <- lme4:::deriv12(centDfun, mod$opt$par)
+eigen(hh$Hessian)$values
+1.96 * sqrt(diag(solve(0.5*hh$Hessian)))
 
+library(numDeriv)
+hhRich <- hessian(centDfun, mod$opt$par)
+eigen(hh$Hessian)$values
+cov2cor(solve(0.5 * hh$Hessian))
+1.96 * sqrt(diag(solve(0.5*hhRich)))
+
+centDfun
+
+cbind(estimated = mod$opt$par, # estimated parameters
+      true = c(covar = covarSim, fixef = fixefSim)) # true parameters
+
+
+
+vc <- vcov(mod, FALSE)
+
+1.96 * (sqrt(diag(vc))[1:3]/covar(mod))
+log(covar(mod))
+
+optPar <- mod$opt$par
+pfun <- local({
+    whichPar <- 1
+    op <- optPar[-whichPar]
+    function(par) {
+        fn <- function(op) {
+            parNow <- numeric(length(op) + 1)
+            parNow[whichPar] <- par
+            parNow[-whichPar] <- op
+            centDfun(parNow)
+        }
+        opt <- bobyqa(op, fn, lower = mod$lower[-whichPar],
+                      control = list(iprint = 4L, maxfun = 500))
+        op <<- opt$par
+        return(opt$fval)
+    }
+})
+
+grd <- seq(mod$opt$par[whichPar] - 0.1, mod$opt$par[whichPar] + 0.1, length = 10)
+pro <- sapply(grd, pfun)
+plot(grd, pro, type = "o")
+
+mod2 <- bobyqa(mod$opt$par, mod$dfun, lower = mod$lower,
+               control = list(iprint = 4L))
+mod2 <- bobyqa(mod2$par, mod$dfun, lower = mod$lower,
+               control = list(iprint = 4L))
+
+mod$opt$par
+mod2$par
+
+plot(interpSpline(grd, pro))
+(cs <- c(0, qchisq(0.95, 1)))
+abline(h = cs)
+abline(v = mod$opt$par[whichPar])
+abline(v = c(covarSim, fixefSim)[whichPar])
+abline(v = mod$opt$par[whichPar] + c(-1, 1) * 1.96 * sqrt(diag(solve(0.5*hh$Hessian)))[whichPar])
+xx <- seq(min(grd), max(grd), length = 100)
+mQuad <- lm(pro ~ grd + I(grd^2))
+lines(xx, predict(mQuad, newdata = data.frame(grd = xx)))
+
+interpSpline(grd, pro)
+
+
+interpSpline(grd, pro)$coef
 ## ------------------------------------------------------------------------
 cbind(estimated = mod$opt$par, # estimated parameters
       true = c(covar = covarSim, fixef = fixefSim)) # true parameters
